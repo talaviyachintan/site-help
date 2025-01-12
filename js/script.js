f1ee6c33bcff4d07d3e0e4d60d6e8430a20efc84
talaviyachintan <140591674+talaviyachintan@users.noreply.github.com>
2025-01-12 18:20:40 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-<script>
-    const links = document.getElementsByTagName('a');
 
-    for (let i = 0; i < links.length; i++) {
-        links[i].target = '_blank';
-    }
-</script>
+const links = document.getElementsByTagName('a');
+
+for (let i = 0; i < links.length; i++) {
+    links[i].target = '_blank';
+}
